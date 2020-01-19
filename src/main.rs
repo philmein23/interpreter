fae9ea17292e5e843fbe1c64a5055ae9caf00f5f
Phillip Nguyen <phillipnguyen@Phillips-MacBook-Pro.local>
2020-01-19 10:35:03 -0800

    updated REPL

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,5 @@ mod token;
 use repl::start;
 
 fn main() {
-    // start();
-
-    #[derive(Debug)]
-    pub enum Statement {
-        Let { name: String, value: Expression },
-    }
-
-    #[derive(Debug)]
-    pub enum Expression {
-        Const(i32),
-    }
-
-    let let_statement = Statement::Let {
-        name: String::from("x"),
-        value: Expression::Const(2),
-    };
-
-    println!("{:?}", let_statement);
+    start();
 }
