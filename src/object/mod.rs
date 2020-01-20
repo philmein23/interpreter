f7f7682cc16c0acaf2e32058283041e3e0e775ac
Phillip Nguyen <phillipnguyen@Phillips-MacBook-Pro.local>
2020-01-19 23:50:40 -0800

    wip evaluator: integers, boolean, and null

---
+++ src/object/mod.rs
@@ -0,0 +1,27 @@
+use std::fmt;
+
+pub enum Object {
+    Integer(i64),
+    Boolean(bool),
+    Null,
+}
+
+impl fmt::Display for Object {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Object::Integer(value) => write!(f, "{}", value),
+            Object::Boolean(value) => write!(f, "{}", value),
+            Object::Null => write!(f, "null"),
+        }
+    }
+}
+
+impl Object {
+    fn type_name(&self) -> &str {
+        match self {
+            Object::Integer(_) => "INTEGER",
+            Object::Boolean(_) => "BOOLEAN",
+            Object::Null => "NULL",
+        }
+    }
+}
